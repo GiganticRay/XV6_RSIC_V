ef3841a2dedfc1cf98a39c9510a6e5370a954c0e
GiganticRay <lc3202683603@gmail.com>
2024-07-09 07:53:45 +0800

    LAB2.1: trace

--- kernel/systrace.c
+++ kernel/systrace.c
@@ -1,6 +1,18 @@
 #include "types.h"
+#include "param.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "proc.h" // depends on spinlock & pagetable_t in riscv.h
+#include "syscall.h"
+#include "defs.h"
 
 uint64 sys_trace(void) {
-  // TODO
+  int traceMask;
+  if (argint(0, &traceMask) < 0) {
+    printf("sys_trace: invalid argument\n");
+    return -1;
+  }
+
+  myproc()->traceMask = traceMask;
   return 0;
 }
